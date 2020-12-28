630fb1119ee1d2b4d218501e503e8f50e6a2e5b2
TheRBZ <benhansen108@gmail.com>
2020-12-28 19:14:03 +0000

    Add comment

--- src/me/therbz/rbznightvision/Main.java
+++ src/me/therbz/rbznightvision/Main.java
@@ -14,6 +14,7 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        // Get configuration, and save the default if there is no file
         getConfig().options().copyDefaults(true);
         saveDefaultConfig();
 
