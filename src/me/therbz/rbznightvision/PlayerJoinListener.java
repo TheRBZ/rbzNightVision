1f04ac52d27e3d71b6254af49a1f897940ac87a2
TheRBZ <benhansen108@gmail.com>
2021-01-11 10:44:40 +0000

    Add @EventHandler

--- src/me/therbz/rbznightvision/PlayerJoinListener.java
+++ src/me/therbz/rbznightvision/PlayerJoinListener.java
@@ -2,6 +2,7 @@ package me.therbz.rbznightvision;
 
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -13,6 +14,7 @@ import java.util.Objects;
 public class PlayerJoinListener implements Listener {
     private final JavaPlugin plugin = RbzNightVision.getPlugin(RbzNightVision.class);
 
+    @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player p = event.getPlayer();
 
